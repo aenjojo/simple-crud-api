355940a2fb3c5d470e4ba8d7cab55085edd27917
aenjojo <josuafernando999@gmail.com>
2023-07-28 15:05:06 +0700

    feat: add button link component

---
+++ src/components/ButtonLink.tsx
@@ -0,0 +1,35 @@
+import NextLink from 'next/link';
+
+type ButtonLinkProps = {
+	href: string;
+	label: string;
+	type?: 'primary' | 'secondary' | 'danger';
+};
+
+export function ButtonLink({ href, label, type = 'primary' }: ButtonLinkProps) {
+	let buttonColor = '';
+	
+	switch(type) {
+		case 'primary':
+			buttonColor = 'bg-emerald-500 text-slate-50 border-emerald-500 hover:bg-emerald-600';
+			break;
+		case 'secondary':
+			buttonColor = 'bg-transparent border-transparent text-slate-700 hover:bg-slate-200';
+			break;
+		case 'danger':
+			buttonColor = 'bg-transparent border-transparent text-red-600 hover:bg-red-200';
+			break;
+	}
+
+	return (
+		<NextLink
+			href={href}
+			className={`
+				px-4 py-2 font-medium border rounded
+				${buttonColor}
+			`}
+		>
+			{label}
+		</NextLink>
+	);
+}
