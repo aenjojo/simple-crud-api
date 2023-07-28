ab39bd0ec9348f1ed33ef12dff1b447b66beb1e9
aenjojo <josuafernando999@gmail.com>
2023-07-28 15:05:49 +0700

    fix: remove post author info

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,24 +3,18 @@ import { Link } from './Link';
 type CardProps = {
 	title: string;
 	content: string;
-	author: string;
 	postId: number;
 };
 
-export function Card({ title, author, content, postId }: CardProps) {
+export function Card({ title, content, postId }: CardProps) {
 	return (
 		<div className={`
 			w-full md:w-80 lg:w-96 p-4 rounded-lg flex flex-col gap-2
 			bg-slate-50 shadow-lg hover:bg-slate-100 hover:shadow-xl
 		`}>
-			<div>
-				<h3 className='line-clamp-2 capitalize'>
-					{title}
-				</h3>
-				<p className='text-sm text-slate-700'>
-					By {author}
-				</p>
-			</div>
+			<h3 className='line-clamp-2 capitalize'>
+				{title}
+			</h3>
 			<div>
 				<p className='line-clamp-3'>
 					{content}
