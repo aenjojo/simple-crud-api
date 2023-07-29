e807829f1c0cb779ada649ba6c53abd1e9d3916b
aenjojo <josuafernando999@gmail.com>
2023-07-29 13:37:05 +0700

    feat: add variant to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 
 type ButtonProps = {
 	label: string;
+	variant?: 'primary' | 'secondary' | 'danger';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ label, ...props }: ButtonProps) {
+export function Button({ label, variant, ...props }: ButtonProps) {
+	let buttonColor = '';
+	
+	switch(variant) {
+		case 'primary':
+			buttonColor = 'bg-emerald-500 text-slate-50 border-emerald-500 hover:bg-emerald-600';
+			break;
+		case 'secondary':
+			buttonColor = 'bg-transparent border-transparent text-slate-700 hover:bg-slate-200';
+			break;
+		case 'danger':
+			buttonColor = 'bg-transparent border-transparent text-red-600 hover:bg-red-200';
+			break;
+	}
+
 	return (
 		<button
-			className={`
-				px-4 py-2 font-medium border rounded
-				bg-emerald-500 text-slate-50 border-emerald-500 hover:bg-emerald-600
-			`}
+			className={`px-4 py-2 font-medium border rounded ${buttonColor}`}
 			{...props}
 		>
 			{label}
