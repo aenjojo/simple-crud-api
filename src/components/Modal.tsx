b5c84d8f0ea9abc4b468eec72c68dd966a039a6f
aenjojo <josuafernando999@gmail.com>
2023-07-29 13:38:29 +0700

    fix: set button to only close modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,12 @@
-import { ButtonLink } from './ButtonLink';
+import { Button } from './Button';
 
 type ModalProps = {
 	message: string;
 	buttonLabel: string;
-	closeHref: string;
+	closeFunc(): void;
 }
 
-export function Modal({ message, buttonLabel, closeHref }: ModalProps) {
+export function Modal({ message, buttonLabel, closeFunc }: ModalProps) {
 	return (
 		<div className='absolute m-0 p-0 top-0 left-0 w-screen h-screen bg-slate-700 bg-opacity-30 flex justify-center items-center'>
 			<div className='fixed p-4 w-64 md:w-96 rounded-lg bg-slate-50 shadow-xl'>
@@ -15,10 +15,10 @@ export function Modal({ message, buttonLabel, closeHref }: ModalProps) {
 					<p>{message}</p>
 				</div>
 				<div className='flex flex-row justify-end mt-4'>
-					<ButtonLink
+					<Button
 						label={buttonLabel}
-						type='primary'
-						href={closeHref}
+						variant='primary'
+						onClick={closeFunc}
 					/>
 				</div>
 			</div>
